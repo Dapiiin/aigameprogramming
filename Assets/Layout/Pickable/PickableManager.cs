b16833ef5ee639e6afbf5c6f3a1aa76e56b28f79
Dapin <dhafinsalman10@gmail.com>
2026-04-15 08:19:45 +0700

    init
    Pickable Object Update

---
+++ Assets/Layout/Pickable/PickableManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableManager : MonoBehaviour
+{
+    private List<Pickable> _pickableList = new List<Pickable>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitPickableList();
+    }
+
+    private void InitPickableList()
+    {
+        Pickable[] pickableObjects = GameObject.FindObjectsOfType<Pickable>();
+        for (int i = 0; i < pickableObjects.Length; i++)
+        {
+            _pickableList.Add(pickableObjects[i]);
+            pickableObjects[i].OnPicked += OnPickablePicked;
+        }
+        Debug.Log("Pickable List: " + _pickableList.Count);
+    }
+
+    private void OnPickablePicked(Pickable pickable)
+    {
+        _pickableList.Remove(pickable);
+        Debug.Log("Pickable List: " + _pickableList.Count);
+
+        if (_pickableList.Count <= 0)
+        {
+            Debug.Log("Win");
+        }
+    }
+
+}
